0cc8476478b03e8a59b2020d8dad25c705b98139
victor1810 <victor181078@gmail.com>
2022-09-29 16:36:54 +0300

    add setteres and getters

--- src/ClassesAndObject.java
+++ src/ClassesAndObject.java
@@ -1,22 +1,38 @@
 public class ClassesAndObject {
     public static void main(String[] args) {
-        System.out.println();
         Person person1 = new Person();
-        person1.setNameAndAge("Роман", 50);
+        person1.setName("");
+        person1.setAge(-7);
+        System.out.println("Выводим значение в main методе: " + person1.getName());
+        System.out.println("Выводим значение в main методе: " + person1.getAge());
         person1.speak();
-        person1.calculateYears();
-        System.out.println();
-        Person person2 = new Person();
-        String s2 = "Вова";
-        person2.setNameAndAge(s2, 20);
-        person2.speak();
-        person2.calculateYears();
     }
 }
 
 class Person {
-    String name;
-    int age;
+    private String name;
+    private int age;
+
+    public void setName(String userName) {
+        if (userName.isEmpty()){
+            System.out.println("Ты не ввёл своё имя");
+        }
+        else {name = userName;}
+    }
+    public String getName(){
+        return  name;
+    }
+    public void setAge(int userAge){
+        if (userAge < 0) {
+            System.out.println("Возраст не может быть отрицательным");
+        }
+        else {age = userAge;}
+    }
+    public int getAge(){
+        return age;
+    }
+
+
 
     void setNameAndAge(String username, int userage) {
         name = username;
