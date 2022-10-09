203a81608d83845ad7c776b80411129f40d10456
victor1810 <victor181078@gmail.com>
2022-10-09 21:26:40 +0300

    change main

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
 public class Main {
     public static void main(String[] args) {
-        Random random = new Random();
-        var cube = new ArrayList<Integer>(List.of(0, 0, 0, 0, 0, 0));
-        int count = 20;
-
-        while (count > 0) {
-            int position = random.nextInt(0, 6);
-            Integer el = cube.get(position);
-            cube.set(position, el + 1);
-            count -= 1;
-        }
-        System.out.println(cube);
     }
 }
--- src/Tasks.java
+++ src/Tasks.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Tasks {
     public static void main(String[] args) {
         int[] array = {51,136,387};
-        int k;
         for (int i:array) {
             System.out.print(i + " ");
         }
